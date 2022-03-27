778cfec573ca0768881356affb4561b2d0fa12c5
Aras Uludag <arasuludag@gmail.com>
2022-03-27 17:36:40 +0300

    first commit

---
+++ NodeMCU_Solar_Slave.ino
@@ -0,0 +1,151 @@
+#include <ESP8266WiFi.h>
+#include <WiFiClientSecure.h>
+#include <ESP8266WebServer.h>
+#include <ESP8266HTTPClient.h>
+
+#include <ArduinoJson.h>
+
+#include "secrets.h"
+
+#define FASTLED_ALLOW_INTERRUPTS 0
+#include <FastLED.h>
+
+#define LED_PIN 16
+#define NUM_LEDS 39
+CRGB leds[NUM_LEDS];
+
+#define SYSTEM_SIZE 8000  // Peak wattage of the solar system.
+
+const char *ssid = SECRET_SSID;  // WIFI Stuff
+const char *password = SECRET_WIFI_PASSWORD;
+
+//Your Domain name with URL path or IP address with path
+String serverName = LOCAL_SERVER;
+
+// the following variables are unsigned longs because the time, measured in
+// milliseconds, will quickly become a bigger number than can be stored in an int.
+unsigned long lastTime = 0;
+// Timer set to 10 minutes (600000)
+//unsigned long timerDelay = 600000;
+// Set timer to 5 seconds (5000)
+unsigned long timerDelay = 5000;
+
+void setup() {
+  FastLED.addLeds<WS2812, LED_PIN, GRB>(leds, NUM_LEDS);
+  FastLED.setBrightness(64);
+
+  Serial.begin(115200); 
+
+  WiFi.begin(ssid, password);
+  Serial.println("Connecting");
+  while(WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("Connected to WiFi network with IP Address: ");
+  Serial.println(WiFi.localIP());
+ 
+  Serial.println("Timer set to 5 seconds (timerDelay variable), it will take 5 seconds before publishing the first reading.");
+
+}
+
+void loop() {
+  // Send an HTTP POST request depending on timerDelay
+  if ((millis() - lastTime) > timerDelay) {
+    //Check WiFi connection status
+    if(WiFi.status()== WL_CONNECTED){
+      WiFiClient client;
+      HTTPClient http;
+
+      String serverPath = serverName;
+      
+      // Your Domain name with URL path or IP address with path
+      http.begin(client, serverPath.c_str());
+      
+      // Send HTTP GET request
+      int httpResponseCode = http.GET();
+      
+      if (httpResponseCode>0) {
+        Serial.print("HTTP Response code: ");
+        Serial.println(httpResponseCode);
+        String payload = http.getString();
+
+        DynamicJsonDocument res(1024);
+        deserializeJson(res, payload);
+        JsonObject obj = res.as<JsonObject>();
+
+      // {"ratedPower":9.1,"gridPower":249.0,"relay2Power":0.0,"feedInPower":5.0,"relay1Power":0.0,"batPower1":0.0}
+
+      if (!obj.containsKey("gridPower")) {
+        Serial.println("Couldn't get JSON data.");
+        return;
+      }
+
+      String gridPower = obj["gridPower"];
+      String feedInPower = obj["feedInPower"];
+
+      Light(feedInPower.toInt(), gridPower.toInt());
+
+
+      }
+      else {
+        Serial.print("Error code: ");
+        Serial.println(httpResponseCode);
+      }
+
+      // Free resources
+      http.end();
+
+    }
+    else {
+      Serial.println("WiFi Disconnected");
+    }
+    lastTime = millis();
+  }
+}
+
+void Light(int FeedIn, int Grid) {
+
+  int feedInMapped = map(FeedIn, -SYSTEM_SIZE, SYSTEM_SIZE, -NUM_LEDS, NUM_LEDS);
+  int gridMapped = map(Grid, 0, SYSTEM_SIZE, 0, NUM_LEDS);
+
+  Serial.print("Grid Mapped: ");
+  Serial.println(gridMapped);
+
+  Serial.print("FeedIn Mapped: ");
+  Serial.println(feedInMapped);
+
+  Serial.println("__________");
+
+  // {"ratedPower":9.1,"gridPower":249.0,"relay2Power":0.0,"feedInPower":5.0,"relay1Power":0.0,"batPower1":0.0}
+  // Solar usage is gridPower - feedInPower when feedInPower > 0.
+  int solarHomeUsage = FeedIn > 0 ? gridMapped - feedInMapped : gridMapped;
+
+  for (int i = 0; i < NUM_LEDS; i++) {
+
+    if (i < solarHomeUsage) {
+      leds[i] = CRGB(0, 255, 200);
+      delay(100);
+      FastLED.show();
+      continue;
+    }
+
+    else if (FeedIn < 0 && i < -feedInMapped + solarHomeUsage) {
+      leds[i] = CRGB(255, 20, 0);
+      delay(100);
+      FastLED.show();  // These seperate .show()s are because of some sort of bug with ESP8266.
+    }
+
+    else if (FeedIn > 0 && i < feedInMapped + solarHomeUsage) {
+      leds[i] = CRGB(255, 70, 0);
+      delay(100);
+      FastLED.show();
+    }
+
+    else {
+      leds[i] = CRGB(0, 0, 0);
+      FastLED.show();
+    }
+  }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+https://github.com/arasuludag/Tommatech-Inverter-Visualiser-with-ARGB-LED - This is for visualization of Tommatech inverter electricity yield and grid usage using ESP8266.
+
+This is the local slave for the same thing. In any case.
